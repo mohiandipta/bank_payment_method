1cab0926c66713bd7fcef550e49eabe862622d1d
mohiandipta <mohianmostofa@gmail.com>
2022-09-11 11:51:27 +0600

    components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
 import React from "react";
+import styles from "./style";
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
-    <h1>
-      Hello, World!
-    </h1>
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        NAVBAR
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        HERO
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        Stats
+        Business
+        Billing
+        CardDeal
+        Testimonial
+        Client
+        CTA
+        Footer
+      </div>
+    </div>
+
   </div>
 )
 
