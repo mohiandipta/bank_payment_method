af7ec264501f5ffcaa06c1ea9de59b6082944492
mohiandipta <mohianmostofa@gmail.com>
2022-09-14 16:55:17 +0600

    business, billing done

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,11 +1,10 @@
 import React from "react"
+import styles from "../style"
 
-const Button = () => {
-    return (
-        <div>
-            Button
-        </div>
+const Button = () => (
+        <button type="button" className={`py-4 px-6 bg-blue-grdient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}>
+            Get Started
+        </button>
     )
-}
 
 export default Button
--- src/Components/CardDeal.jsx
+++ src/Components/CardDeal.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { card } from '../Assets'
+import styles, {layout} from '../style'
+import Button from './Button'
 
-const CardDeal = () => {
-  return (
+const CardDeal = () => (
     <div>CardDeal</div>
   )
-}
 
 export default CardDeal
